07a8bf172fc1bd6340ffc4def5098df3f03b6b92
Alexis LG <alex-87@users.noreply.github.com>
2017-06-05 11:50:28 +0200

    Updating major version

--- demo/include/Client.hh
+++ demo/include/Client.hh
@@ -2,9 +2,9 @@
 #ifndef CLIENT_INCLUDE_CLIENT_HH_
 #define CLIENT_INCLUDE_CLIENT_HH_
 
-#define VERSION_MAJOR 0
-#define VERSION_MINOR 17
-#define VERSION_BUILD 1
+#define VERSION_MAJOR 2
+#define VERSION_MINOR 0
+#define VERSION_BUILD 0
 
 #define NewAlgorithm(a, b, c) boost::shared_ptr<AlgorithmeAbstrait> a( new b( c ) );
 #define NewAlgorithm2(a, b, c, d) boost::shared_ptr<AlgorithmeAbstrait> a( new b( c , d ) );
